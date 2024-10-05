f7797061f66ac97bcb5d028d9aa873e7176c46a1
Atomooooioio <mariocraft987dev+githubacc2@gmail.com>
2024-10-05 11:21:51 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,28 @@
 using namespace std;
 
 int main() {
-  double first, last;
+  int first;
+  int last;
+  string type;
   
   cout << "------------------------------------------------------\n";
   cout << "Welcome to the calculator\nEnter the first variable:\n";
   cin >> first;
   cout << "Enter the second variable:\n";
   cin >> last;
-  cout << "The sum is " << first + last << "!";
+  cout << "Enter the math type e.g. plus, minus, times, divide:\n";
+  cin >> type;
+  if (type == "plus") {
+    cout << "The sum is " << first + last << "!";
+  } else if (type == "minus") {
+    cout << "The sum is " << first - last << "!";
+  } else if (type == "times") {
+    cout << "The sum is " << first * last << "!";
+  } else if (type == "divide") {
+    cout << "The sum is " << first / last << "!";
+  } else {
+    cout << "That isn't a valid math type!";
+  }
+  
   return 0;
 }
